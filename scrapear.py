fe41ce4834d50cb6890d797feb1999f435b674e0
Diego18oo <diegorodriguez300806@gmail.com>
2025-02-04 18:09:27 -0600

    Solucionando html

--- scrapear.py
+++ scrapear.py
@@ -12,13 +12,27 @@ fbref = sfc.FBref()
 def sacar_tabla(liga):
     tabla_base = fbref.scrape_league_table(year='2024-2025', league=liga)
     tabla = tabla_base[0][['Rk','Squad', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'Pts']]
-    return tabla
+    tabla_formateada = [
+        {
+            "rk": row["Rk"],
+            "club": row["Squad"],
+            "pj": row["MP"],
+            "v": row["W"],
+            "e": row["D"],
+            "d": row["L"],
+            "ga": row["GF"],
+            "gc": row["GA"],
+            "pts": row["Pts"]
+        }
+        for _, row in tabla.iterrows()
+    ]
+    return tabla_formateada
 
 
 if __name__ == "__main__":
     print ("\nVer Ligas \n")
     liga = input("\nIngresa una liga:")
-    tabla = sacar_tabla(liga)
+    tabla_formateada = sacar_tabla(liga)
     print("\n")
-    pprint(tabla)
+    pprint(tabla_formateada)
 
