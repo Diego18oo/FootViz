c24d8d7f7a5fd879420422889c78ace863715641
Diego18oo <diegorodriguez300806@gmail.com>
2025-02-05 09:56:20 -0600

    Por favor funciona

--- scrapear.py
+++ scrapear.py
@@ -6,11 +6,24 @@ import ScraperFC as sfc
 from flask import Flask, render_template, request, jsonify
 from pprint import pprint
 from io import BytesIO
+import requests
+
+# Parchear requests para registrar todas las llamadas
+original_get = requests.get
+
+def custom_get(*args, **kwargs):
+    print(f"Solicitud GET a: {args[0]}")
+    return original_get(*args, **kwargs)
+
+requests.get = custom_get
 
 fbref = sfc.FBref()
 
 def sacar_tabla(liga):
+    print(f"Obteniendo tabla para: {liga}")
     tabla_base = fbref.scrape_league_table(year='2024-2025', league=liga)
+    print(f"Tabla obtenida para {liga}: {len(tabla_base[0])} equipos")
+
     tabla = tabla_base[0][['Rk','Squad', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'Pts']]
     tabla_formateada = [
         {
